47a53e827bc6903102e5976834bc16bff66bc597
sona <ohsw17@naver.com>
2024-03-29 17:47:10 +0900

    move global.css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import Navigation from "@/components/Navigation";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
